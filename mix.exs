a9179aef5c8814763e77059c91e38f467d67224d
Dawei Ma <madawei2699@gmail.com>
2019-01-28 13:55:08 +0800

    update to v0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Git2pdf.MixProject do
   def project do
     [
       app: :git2pdf,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.7",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
